618d5d49a3d91c1ce79bf0b55ee168742760c690
Saul Corona <saulcoronascs@gmail.com>
2021-10-24 12:31:46 -0500

    Operaciones de asignación y aritmeticas

---
+++ 5.- Condicionales/condicionales.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/**CONDICIONALES Y OPERACIONES BASICAS**/
+
+//Operaciones de asignación y aritmeticas
+
+let b = 10
+var a = 5
+
+a = b // igualamos el valor de a a 10 por eso en el if si es true
+
+let (x,y) = (1,2)
+
+if a == b {
+    print("Los valos de a y b son iguales")
+}
+
+1+2
+5-3
+2*3
+10.0/2.5
+
+"Hello" + "world"
+
+9/4
+9*4
+
+9 == 4*2+1
+
+-9*4
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+let resto = 17 % 5
